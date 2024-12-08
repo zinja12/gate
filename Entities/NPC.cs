b3323bfba437e542fb4ad17f1f3f5c93fd08af92
zinja12 <x3zinja12@gmail.com>
2024-12-08 15:28:06 -0500

    update npc animations to match up with the new player animations

--- Entities/NPC.cs
+++ Entities/NPC.cs
@@ -117,6 +117,120 @@ namespace gate.Entities
             /*END PARTICLE SYSTEMS*/
         }
 
+        public override void update_animation(GameTime gameTime) {
+            //calculate angle
+            angle = MathHelper.ToDegrees((float)Math.Atan2(direction.Y, direction.X));
+            //update walk animation if moving
+            if (moving) {
+                //update animation for movement
+                int calculated_animation_case = Constant.TranslateAngleToCompassDirection(angle, MathHelper.ToDegrees(rotation));
+                //handle animation sticking so that animations don't rubber-band as quickly leading to jank
+                if (calculated_animation_case != animation_case && animation_movement_swap_elapsed >= animation_movement_swap_threshold) {
+                    animation_case = calculated_animation_case;
+                    animation_movement_swap_elapsed = 0f;
+                }
+                switch (animation_case) {
+                    case 0: //up - north
+                        walk_animation.set_y_offset((int)nightmare_size*3);
+                        break;
+                    case 1: //up right - ne
+                        walk_animation.set_y_offset((int)nightmare_size*6);
+                        break;
+                    case 2: //right - east
+                        walk_animation.set_y_offset((int)nightmare_size*1);
+                        break;
+                    case 3: //down right - se
+                        walk_animation.set_y_offset((int)nightmare_size*5);
+                        break;
+                    case 4: //down - south
+                        walk_animation.set_y_offset((int)nightmare_size*2);
+                        break;
+                    case 5: //down left - sw
+                        walk_animation.set_y_offset((int)nightmare_size*4);
+                        break;
+                    case 6: //left - west
+                        walk_animation.set_y_offset((int)nightmare_size*0);
+                        break;
+                    case 7: //up left - nw
+                        walk_animation.set_y_offset((int)nightmare_size*7);
+                        break;
+                    default:
+                        Console.WriteLine("Nightmare AI: Default movement vector case.");
+                        break;
+                }
+                // keep walk animation updated when moving
+                walk_animation.Update(gameTime);
+            } else if (melee_attack) {
+                switch (Constant.TranslateAngleToCompassDirection(angle, MathHelper.ToDegrees(rotation))) {
+                    case 0: // up - north
+                        attack_animation.set_y_offset((int)nightmare_size*2*3);
+                        break;
+                    case 1: //up right - ne
+                        attack_animation.set_y_offset((int)nightmare_size*2*1);
+                        break;
+                    case 2: //right - east
+                        attack_animation.set_y_offset((int)nightmare_size*2*1);
+                        break;
+                    case 3: //down right - se
+                        attack_animation.set_y_offset((int)nightmare_size*2*1);
+                        break;
+                    case 4: //down - south
+                        attack_animation.set_y_offset((int)nightmare_size*2*2);
+                        break;
+                    case 5: //down left - sw
+                        attack_animation.set_y_offset((int)nightmare_size*2*0);
+                        break;
+                    case 6: //left - west
+                        attack_animation.set_y_offset((int)nightmare_size*2*0);
+                        break;
+                    case 7: //up left - nw
+                        attack_animation.set_y_offset((int)nightmare_size*2*0);
+                        break;
+                    default:
+                        Console.WriteLine("Nightmare AI: Default animation case");
+                        attack_animation.set_y_offset((int)nightmare_size*2*0);
+                        break;
+                }
+                // keep attack animation updated when melee attack is active
+                attack_animation.Update(gameTime);
+            } else {
+                //update idle animation based on last direction
+                switch (last_movement_vector_idx) {
+                    case 0: //up
+                        idle_animation.set_y_offset((int)nightmare_size*11);
+                        break;
+                    case 1: //up right
+                        idle_animation.set_y_offset((int)nightmare_size*14);
+                        break;
+                    case 2: //right
+                        idle_animation.set_y_offset((int)nightmare_size*9);
+                        break;
+                    case 3: //down right
+                        idle_animation.set_y_offset((int)nightmare_size*13);
+                        break;
+                    case 4: //down
+                        idle_animation.set_y_offset((int)nightmare_size*10);
+                        break;
+                    case 5: //down left
+                        idle_animation.set_y_offset((int)nightmare_size*12);
+                        break;
+                    case 6: //left
+                        idle_animation.set_y_offset((int)nightmare_size*8);
+                        break;
+                    case 7: //up left
+                        idle_animation.set_y_offset((int)nightmare_size*15);
+                        break;
+                    default:
+                        Console.WriteLine("Nightmare AI: Default movement vector case.");
+                        break;
+                }
+                //keep idle animation updated when not moving
+                idle_animation.Update(gameTime);
+            }
+            //increase animation timer
+            animation_movement_swap_elapsed += (float)gameTime.ElapsedGameTime.TotalMilliseconds;
+        }
+
         public virtual void update_hurtbox_activity(GameTime gameTime, float rotation) {
             //set hurtbox active again
             if (!hurtbox_active) { //check if hurtbox is not active
